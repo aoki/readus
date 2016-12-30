f73d7b2cd791386e0a4633c4da9c8705699c9405
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2016-12-30 16:34:47 +0900

    Fix xo warnings

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
 'use strict';
-
 const electron = require('electron');
+const path = require('path');
+
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-var mainWindow = null;
+let mainWindow = null;
 
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin')
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
+  }
 });
 
-app.on('ready', function() {
+app.on('ready', () => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     titleBarStyle: 'hidden',
     transparent: true
   });
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
 
-  mainWindow.on('closed', function() {
+  mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
